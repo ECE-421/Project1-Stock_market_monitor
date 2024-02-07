217db3d54870a9af17e3107b526c215022078907
Logan <lkosedy24@gmail.com>
2024-02-07 16:58:12 -0700

    moved api stuff to seperate module

--- stock_monitor/src/main.rs
+++ stock_monitor/src/main.rs
@@ -1,29 +1,8 @@
-use yahoo_finance_api as yahoo;
-use time::{macros::datetime, OffsetDateTime};
-use tokio_test;
-
-
-fn get_quote_history(ticker_symbol: &str, month_offset: i64 ) -> Vec<yahoo::Quote> {
-   let end = OffsetDateTime::now_utc();
-   let start = end - time::Duration::days(30 * month_offset); // if month_offset = 6 it would be 30 * 6 days
-
-   let provider = yahoo::YahooConnector::new();
-   match tokio_test::block_on(provider.get_quote_history(ticker_symbol, start, end)) {
-      Ok(resp) => {
-         let quotes = resp.quotes().unwrap();
-         return quotes
-      }
-      Err(err) => {
-         eprintln!("Error: {:?}", err);
-         return Vec::new()
-      }
-   }
-}
-
+mod yahoo_api;
 
 fn main() {
-   let symbol = "AAPL";
+   let symbol = "AAPaL";
 
-   let quotes = get_quote_history(symbol, 6);
+   let quotes = yahoo_api::get_quote_history(symbol, 6);
    println!("Quotes for {}: {:?}", symbol, quotes);
 }
